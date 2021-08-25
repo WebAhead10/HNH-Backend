49d91279fb3691db14caa7eec66a77c668e6e210
anahseen <anahseen@outlook.com>
2021-08-25 12:56:12 +0300

    login backend

--- router.js
+++ router.js
@@ -1,4 +1,12 @@
 const router = require("express").Router()
+const signup = require("./controllers/signup.controller")
+const login = require("./controllers/login.controller")
+const allUsers = require("./controllers/users.controller")
+const verifyUser = require("./middleware/verifyUser")
 
+router.post("/signup", signup)
+router.get("/signup", signup)
+router.post("/login", login)
+router.get("/users", verifyUser, allUsers)
 
 module.exports = router
