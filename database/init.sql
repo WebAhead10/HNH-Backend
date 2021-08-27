8d0ddad67556021d66d504563104b5d0b968fb6a
noor-sawaed <n.swa3ed.swa3ed@gmail.com>
2021-08-27 15:31:46 +0300

    update init.sql

--- database/init.sql
+++ database/init.sql
@@ -1,6 +1,6 @@
 BEGIN;
-
-DROP TABLE IF EXISTS users ,workers CASCADE;
+DROP TABLE IF EXISTS users,
+workers,appointments CASCADE;
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
@@ -13,22 +13,77 @@ CREATE TABLE workers (
     fullname VARCHAR(255) NOT NULL,
     jobtitle VARCHAR(255) NOT NULL,
     img VARCHAR(255) NOT NULL,
-    ratenumber VARCHAR NOT NULL,
+    ratenumber INTEGER NOT NULL,
     phonenum VARCHAR(255) NOT NULL,
     info VARCHAR(255) NOT NUll,
-
     created_at DATE DEFAULT CURRENT_TIMESTAMP
 );
-INSERT INTO workers (fullname,jobtitle,img,ratenumber,phonenum,info) VALUES
-(
-'mario saliba','full stack developer','https://media-exp1.licdn.com/dms/image/C4D03AQHc-4FfvvSAFQ/profile-displayphoto-shrink_800_800/0/1594492798405?e=1634774400&v=beta&t=datBhT_arhrHCuMq_DuPKgck3edmzrmvkIEy1F6NQ0A','4','05050505050','im a smart guy, Mario is a senior fullstack web developer that have alot of experience with frontend & backend development.'
-),
-(
-'mario saliba','plumer','https://media-exp1.licdn.com/dms/image/C4D03AQHc-4FfvvSAFQ/profile-displayphoto-shrink_800_800/0/1594492798405?e=1634774400&v=beta&t=datBhT_arhrHCuMq_DuPKgck3edmzrmvkIEy1F6NQ0A','3','05050505050','im a fool guy, Mario is a senior fullstack web developer that have alot of experience with frontend & backend development.'
-),
-(
-'mario saliba','carpinter','https://media-exp1.licdn.com/dms/image/C4D03AQHc-4FfvvSAFQ/profile-displayphoto-shrink_800_800/0/1594492798405?e=1634774400&v=beta&t=datBhT_arhrHCuMq_DuPKgck3edmzrmvkIEy1F6NQ0A','2','05050505050','im an idiot guy, Mario is a senior fullstack web developer that have alot of experience with frontend & backend development.'
+CREATE TABLE appointments (
+    workerId INTEGER NOT NULL,
+    jobYear varchar(5) NOT NULL,
+    jobMonth varchar(5) NOT NULL,
+    jobDay varchar(5) NOT NULL,
+    jobHours varchar(5) NOT NULL,
+    jobMinutes varchar(5) NOT NULL,
+    PRIMARY KEY (
+        workerId,
+        jobYear,
+        jobMonth,
+        jobDay,
+        jobHours,
+        jobMinutes
+    )
 );
 
+INSERT INTO workers (
+        fullname,
+        jobtitle,
+        img,
+        ratenumber,
+        phonenum,
+        info
+    )
+VALUES (
+        'mario saliba',
+        'full stack developer',
+        'https://media-exp1.licdn.com/dms/image/C4D03AQHc-4FfvvSAFQ/profile-displayphoto-shrink_800_800/0/1594492798405?e=1634774400&v=beta&t=datBhT_arhrHCuMq_DuPKgck3edmzrmvkIEy1F6NQ0A',
+        4,
+        '05050505050',
+        'im a smart guy, Mario is a senior fullstack web developer that have alot of experience with frontend & backend development.'
+    ),
+    (
+        'hassan hasuna',
+        'plumer',
+        'https://avatars.githubusercontent.com/u/82958594?v=4',
+        3,
+        '05050505050',
+        'im a fool guy, Mario is a senior fullstack web developer that have alot of experience with frontend & backend development.'
+    ),
+    (
+        'noor sawaed',
+        'carpinter',
+        'https://avatars.githubusercontent.com/u/83413600?v=4',
+        2,
+        '05050505050',
+        'im an idiot guy, Mario is a senior fullstack web developer that have alot of experience with frontend & backend development.'
+    );
+-- -- SELECT * FROM workers;
+-- INSERT INTO appointments (
+--         workerId,
+--         jobYear,
+--         jobMonth,
+--         jobDay,
+--         jobHours,
+--         jobMinutes
+--     )
+-- VALUES (1,,),
 
+
+-- INSERT INTO users (username, age, location) VALUES
+--   ('Sery1976', 28, 'Middlehill, UK'),
+--   ('Notne1991', 36, 'Sunipol, UK'),
+--   ('Moull1990', 41, 'Wanlip, UK'),
+--   ('Spont1935', 72, 'Saxilby, UK'),
+--   ('Precand', 19, 'Stanton, UK')
+-- ;
 COMMIT;
